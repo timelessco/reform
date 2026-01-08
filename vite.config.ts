08c1c926bf1be460e5423efa55262407384b870f
Vijayabaskar <vijayabaskar@gmail.com>
2026-01-08 18:13:31 +0530

    chore: deployment fix

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,9 @@ import { nitro } from 'nitro/vite'
 const config = defineConfig({
   plugins: [
     devtools(),
-    nitro(),
+    nitro({
+      
+    }),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
